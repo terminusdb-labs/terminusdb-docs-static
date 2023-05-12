d3080a059c2fc40c9e9e55d298a005ebf58f71e9
Robin de Rooij <rderooij685@gmail.com>
2023-05-12 15:43:11 +0200

    Fix menu with new structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,62 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-const TerminusClient = require("@terminusdb/terminusdb-client");
 const inter = Inter({ subsets: ['latin'] })
+import axios from 'axios';
 
-function renderMenuItems(menuItems) {
-    console.log(menuItems)
-    return menuItems.map(x => {
-        let url = x.page[0].split("/")[1]
-        return (
-        <li><a href={url}>{x.label}</a></li>)
+function renderMenu(menu) {
+    const menuItems = menu.Level1.map(level1 => {
+        const level2Items = level1.Level2.map(level2 => {
+            const level3Items = level2.Level3.map(level3 => {
+                return <li><a href={level3.Menu3Page.slug}>{level3.Menu1Label}</a></li>
+            })
+            return <li><a href={level2.Menu2Page.slug}>{level2.Menu2Label}</a><ul>{ level3Items }</ul></li>
+        })
+        return <li><a href={level1.Menu1Page.slug}>{level1.Menu1Label}</a><ul>{ level2Items }</ul></li>
     })
+    return menuItems
 }
 
 export default function Home(params) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="menu">
           <ul>
-          { renderMenuItems(params.menuItems) }
+          { renderMenu(params.menu) }
           </ul>
+          </div>
     </main>
   )
 }
 
 export async function getStaticProps({ params }) {
-const client = new TerminusClient.WOQLClient('https://cloud-dev.terminusdb.com/TerminatorsX',
-                                             {user:"robin@terminusdb.com",
-                                              organization:'TerminatorsX',
-                                              db: "terminusCMS_docs",
-                                              token: process.env.TERMINUSDB_API_TOKEN})
-    const query = {
-        "@type": "MenuItem"
+    const config = {
+        headers: { Authorization: `Token ${process.env.TERMINUSDB_API_TOKEN}` }
+    };
+    const req = await axios.post('https://cloud-dev.terminusdb.com/TerminatorsX/api/graphql/TerminatorsX/terminusCMS_docs', {
+        query: `query {
+  Menu {
+    MenuTitle
+    Level1 {
+      Menu1Label,
+      Menu1Page {
+        slug
+      },
+      Level2 {
+        Menu2Label,
+        Menu2Page{
+          slug
+        },
+        Level3 {
+          Menu3Label,
+          Menu3Page {
+            slug
+          }
+        }
+      }
     }
-    const docs = await client.getDocument({"@type": "MenuItem", as_list: true, query: query})
-    return { props: { menuItems: docs } }
+  }
+}`
+    }, config)
+    const menu = req.data.data.Menu[0]
+    return { props: { menu: menu } }
 }

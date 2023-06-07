11f109d0ca75a0f9c3ff41722eb0d2f0f533ff26
Kitty Jose <kitty@terminusdb.com>
2023-06-07 10:41:18 +0100

    styling ...

--- src/pages/_body.tsx
+++ src/pages/_body.tsx
@@ -15,7 +15,7 @@ export const BodyContent = ({ entry }) => {
   return <div className="p-4 sm:ml-96 p-4 sm:ml-96 h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
     <div className="pl-20 rounded-lg font-normal">
       <h1 className="font-barlow font-semibold">{entry.document.title.value}</h1>
-      <h2 className="font-barlow font-semibold">{getSubTitle(entry.document)}</h2>
+      <h2 className="font-barlow font-semibold tdb__subtitle">{getSubTitle(entry.document)}</h2>
       <div dangerouslySetInnerHTML={{__html: getHtml(entry)}}/>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        'lexand': ['Lexend Deca'],
+        'lexend': ["Lexend Deca"],
         'barlow': ["Barlow Semi Condensed", "Sans-serif"]
       }
     }

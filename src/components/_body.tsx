0ea0ee855b18920c9757293bf2c5fd1f00c9a803
Kitty Jose <kitty@terminusdb.com>
2023-06-09 08:26:17 +0100

    theme switcher

--- src/components/_body.tsx
+++ src/components/_body.tsx
@@ -15,7 +15,8 @@ export const BodyContent = ({ entry }) => {
   
   //return <div className="flex-auto max-w-4xl min-w-0 pt-6 lg:px-8 lg:pt-8 pb:12 xl:pb-24 lg:pb-16"> 
   return <div className="flex p-4 sm:ml-96 p-4 sm:ml-96 h-screen px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-    <div className="pl-20 rounded-lg font-normal container sm">
+    {/*<div className="pl-20 rounded-lg font-normal container sm">*/}
+    <div className="pl-20 rounded-lg font-normal max-w-4xl">
       <h1 className="font-barlow font-semibold">{entry.document.title.value}</h1>
       <h2 className="font-barlow font-semibold tdb__subtitle">{getSubTitle(entry.document)}</h2>
       <div dangerouslySetInnerHTML={{__html: getHtml(entry)}}/>
--- src/pages/python.tsx
+++ src/pages/python.tsx
@@ -30,8 +30,10 @@ export default function Python( props ) {
 
 	return <>
 		<SideBar {...props}/>
-		<div className="flex p-4 sm:ml-96 p-4 sm:ml-96 h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 fixed">
-    	<div className="pl-20 rounded-lg font-normal container sm">
+		{/*<div className="flex p-4 sm:ml-96 p-4 sm:ml-96 h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 fixed">*/}
+    <div className="flex p-4 sm:ml-96 p-4 sm:ml-96 h-screen px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+   	
+			<div className="pl-20 rounded-lg font-normal max-w-4xl">
 				<h1>{ props.application.name }</h1>
 				{ layout }
 				<OnThisPageContent html={cleanedHtml}/>

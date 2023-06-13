19d1dcc1bff35ff16bc8af78a42f40cf99bd7bfe
Robin de Rooij <rderooij685@gmail.com>
2023-06-13 13:08:56 +0200

    Add dark mode without flicker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,27 +14,15 @@ li.classList.add('active');
 
  
 export default function Document() {
-
-  /** on load function to swap between light & dark mode  */
-  function loadTheme() {
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  }
-
-
-  
   return (
     <Html lang="en">
       <Head>
+        <Script src="/darkmode.js" strategy="beforeInteractive" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/default.min.css" rel="stylesheet"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600&family=Lexend+Deca&display=swap" rel="stylesheet"/>
-        <Script onLoad={loadTheme}/>
       </Head>
       <body className='dark:bg-gray-900 antialiased'>
         {/*<header className="sticky top-0 z-40 flex-none w-full mx-auto bg-white border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800">

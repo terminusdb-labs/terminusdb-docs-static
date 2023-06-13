75caebd4b449e2f7d5ca5c7299356634c60c2c13
Robin de Rooij <rderooij685@gmail.com>
2023-06-13 16:32:23 +0200

    config: use empty string if basePath not set

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASE_PATH || "/",
+  basePath: process.env.BASE_PATH || "",
   output: 'export',
   reactStrictMode: false,
   typescript: {

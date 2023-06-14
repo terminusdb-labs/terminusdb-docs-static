368569b461bf1c34b46b0d8781114bc1378e644a
Robin de Rooij <rderooij685@gmail.com>
2023-06-14 14:05:23 +0200

    Fix index page too

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import axios from 'axios';
 import 'flowbite';
 import { getHtml, getSubTitle, getMenu } from "../utils"
 import { Layout } from "../components/_layout"
+import { useRouter } from 'next/router'
 
 // Connect and configure the TerminusClient
 const client = new TerminusClient.WOQLClient('https://cloud.terminusdb.com/TerminatorsX',
@@ -25,17 +26,21 @@ const client = new TerminusClient.WOQLClient('https://cloud.terminusdb.com/Termi
 )
  
 
-export default function Home(params: { menu: any[], entry: any[] }) {
-
-  let html = getHtml(params.entry)
-  let displayElement = <div dangerouslySetInnerHTML={{__html: html}}/> 
-  return <Layout menu={params.menu} 
-    entry={params.entry}
-    displayElement={displayElement} 
-    html={html}
-    heading={params.entry.document.title.value}
-    subtitle={getSubTitle(params.entry.document)}/>
-
+export default function Home(props: { menu: any[], entry: any[] }) {
+    let html = getHtml(props.entry)
+    const router = useRouter()
+    if (router.basePath != '') {
+      html = html.replaceAll(`<a href="/`, `<a href="${router.basePath}/`)
+    }
+    let displayElement = <div dangerouslySetInnerHTML={{__html: html}}/>
+    return <Layout menu={props.menu} 
+        entry={props.entry}
+        displayElement={displayElement} 
+        html={html}
+        heading={props.entry.document.title.value}
+        subtitle={getSubTitle(props.entry.document)}
+        seo_metadata={props.entry.document.seo_metadata}
+     />
 }
 
 export async function getStaticProps({ params }) {

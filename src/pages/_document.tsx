f2d1bc3ae5623bcf28584997fff07bbf2680ec3e
Robin de Rooij <rderooij685@gmail.com>
2023-06-14 09:59:36 +0200

    Add main to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,7 +27,8 @@ export default function Document() {
       <body className='dark:bg-gray-900 antialiased'>
         {/*<header className="sticky top-0 z-40 flex-none w-full mx-auto bg-white border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800">
           <Nav/>
-        </header>*/}
+          </header>*/}
+        <Main />
         <NextScript/>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></Script>
         <Script id="theme-switcher">handleThemeSwitch();</Script>

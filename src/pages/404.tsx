6c2b20a848685ceee8e9081d1ab8c94d90b10df9
Robin de Rooij <rderooij685@gmail.com>
2023-06-21 11:02:05 +0200

    Change 404 message

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,10 @@ import { Layout } from "../components/_layout"
 import menu from "../menu.json"
 
 export default function Custom404() {
-    const not_found = <p>Could not find the requested page</p>
+    const not_found = <p>
+        We've moved our documentation from Gitbook to TerminusCMS to improve the user experience.
+        Some URLs have changed and we're waiting for the search engines to catch up.
+        Please use the menu to find what you were looking for.</p>
     return <Layout menu={menu}
 //        entry={props.entry}
         displayElement={not_found} 

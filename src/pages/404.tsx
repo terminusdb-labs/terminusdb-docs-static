3c00b695b61f59166ff64cdfe498960c61188842
Oliver <88653154+OJ423@users.noreply.github.com>
2023-06-22 15:53:54 +0100

    change double quote to single

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,8 @@ import menu from "../menu.json"
 
 export default function Custom404() {
     const not_found = <p>
-        We&quot;ve moved our documentation from Gitbook to TerminusCMS to improve the user experience.
-        Some URLs have changed and we&quot;re waiting for the search engines to catch up.
+        We&apos;ve moved our documentation from Gitbook to TerminusCMS to improve the user experience.
+        Some URLs have changed and we&apos;re waiting for the search engines to catch up.
         Please use the menu to find what you were looking for.</p>
     return <Layout menu={menu}
 //        entry={props.entry}

03dbad53dc1fdf3a2fffa8c12977c281b9557d84
Robin de Rooij <rderooij685@gmail.com>
2023-07-06 16:09:38 +0200

    Save changes in a PR

--- utils/parse_to_sections.js
+++ utils/parse_to_sections.js
@@ -4,6 +4,39 @@ const client = new TerminusClient.WOQLClient('https://cloud.terminusdb.com/Termi
                                              {organization:'TerminatorsX', db: 'terminusCMS_docs'})
 client.setApiKey(process.env.TERMINUSDB_API_TOKEN)
 
+
+
+function getChangesUrl (action = 'changes') {
+  const clientCopy = client.copy()
+  clientCopy.connectionConfig.api_extension = 'api/'
+  if (clientCopy.connectionConfig.baseServer) {
+    clientCopy.connectionConfig.server = clientCopy.connectionConfig.baseServer
+  }
+  return clientCopy.connectionConfig.dbBase(action)
+}
+
+async function createCR () {
+  const payload = {
+    original_branch: 'main',
+    name: `CR_indexing__${Date.now()}`,
+    message: 'Create change request for indexing',
+    author: client.user()
+  }
+
+  const createURL = getChangesUrl()
+
+  const result = await client.sendCustomRequest('POST', createURL, payload)
+  return result
+}
+
+async function mergeChangeRequest (currentCR) {
+  // I need to submit the change request before merge it
+  await client.sendCustomRequest('PUT', `${getChangesUrl()}/${currentCR}`, { message: 'Submitted CR', status: 'Submitted' })
+  // I merge the change request,  this will trigger the indexing process
+  return await client.sendCustomRequest('PUT', `${getChangesUrl()}/${currentCR}`, { message: 'MERGED CR', status: 'Merged' })
+}
+
+
 function parseDocument(document) {
   const mdBody = document['body'].value
   const tokens = marked.lexer(mdBody, {})
@@ -24,6 +57,8 @@ function parseDocument(document) {
 }
 
 async function main() {
+  const { changeRequestId, branchName } = await createCR()
+  client.checkout(branchName)
   const docs = await client.getDocument({as_list: true, type: 'Page'})
   const newDocs = []
   for (let doc of docs) {
